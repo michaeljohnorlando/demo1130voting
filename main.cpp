9ed403ffdd01c8636f7f464faa0f709837cdccb0
Michael Orlando <michael.john.orlando@gmail.com>
2017-11-30 12:33:06 -0500

    init

--- main.cpp
+++ main.cpp
@@ -1,15 +1,65 @@
-//
-//  main.cpp
-//  demo1130voting
-//
-//  Created by lucky on 11/30/17.
-//  Copyright © 2017 luck. All rights reserved.
-//
 
-#include <iostream>
+//Liang Orlando
+#include<iostream>
+#include<ctime>
+#include<fstream>
+#include<cstdlib>
+#include <time.h>       /* time */
+
 using namespace std;
 
-int main(int argc, const char * argv[]) {
+
+
+void create_db_and_seed(){
+    //check if the file allready exists
+    ifstream infile("voter_db.txt");
+    if(infile.good()){
+        return;
+    }
     
-    return 0;
+    //create and seed the DB
+    ofstream voter_db;
+    voter_db.open("voter_db.txt");
+    for(int x=0;x<11;x++){
+        voter_db<<rand()%9000 +1<<"\n";
+    }
+    voter_db.close();
 }
+int main_menu_voter_id(){
+    int voter_id;
+    cout<<"    Electrinic Vote \n"
+        <<"Enter voter ID: "; 
+    cin>>voter_id;
+    return voter_id;
+}
+bool serch_db_id(int voter_id){
+    string line;
+    fstream search_db ("voter_db.txt");
+    if(search_db.is_open()){
+        while(getline(search_db,line)){
+            if(stoi(line)==voter_id){
+                search_db.close();
+                return true;
+            }
+        }
+    search_db.close();
+    return false;
+    }
+    
+}
+
+main(){
+    srand(time(0));
+    
+    create_db_and_seed();
+    
+    
+    
+}
+/*
+bool is_file_exist(const char *fileName)
+{
+    std::ifstream infile(fileName);
+    return infile.good();
+}
+*/
