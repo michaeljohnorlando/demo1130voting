d92fa901fabd6d0fca10c21230d0340e506a898c
luckily248 <luck248@163.com>
2017-11-30 11:39:08 -0500

    init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  demo1130voting
+//
+//  Created by lucky on 11/30/17.
+//  Copyright © 2017 luck. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
